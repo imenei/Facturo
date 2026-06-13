fe75f99ae17eb0b731247563167a4ee4c5369349
ghofrane <benlakhdarghofrane7@gmail.com>
2026-06-13 12:09:56 +0100

    fix: rebuild PWA for production

--- frontend/.next/standalone/.next/server/server-reference-manifest.js
+++ frontend/.next/standalone/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"DTOObVmkx6bTbFfhMz8I7eImahvSna66aJVE6P3CSCw=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"zsGj+hDGmT88itc1Ps4EffJbkZkciltewuTOcLZ88CY=\"}"
